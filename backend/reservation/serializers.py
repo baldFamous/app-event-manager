5283f40c11a17da808825888f51c9db930f7fb00
brodriguez <bastian3967a@gmail.com>
2024-04-29 19:08:58 -0400

    add docs of reservation views adn serializers

--- backend/reservation/serializers.py
+++ backend/reservation/serializers.py
@@ -5,10 +5,28 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class ReservationSerializer(serializers.ModelSerializer):
-    # Usamos PrimaryKeyRelatedField para asegurar que el input sea un ID de usuario existente
+    """
+    This is a serializer for the Reservation model.
+
+    It inherits from the ModelSerializer class provided by Django Rest Framework.
+    It includes all fields of the Reservation model, and an additional field 'assistant' which is a foreign key to the User model.
+
+    The serializer also includes a custom validate method, which checks if the assistant is present and has the correct role.
+    And a custom create method, which is used to create a new reservation instance.
+    """
+
+    # The assistant field is a foreign key to the User model
+    # We use PrimaryKeyRelatedField to ensure that the input is an existing user ID
     assistant = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
 
     class Meta:
+        """
+        Meta class for the ReservationSerializer.
+
+        Specifies the model to be serialized and the fields to be included in the serialized representation.
+        Also specifies extra settings for the fields, in this case making certain fields required and not allowing null for the assistant field.
+        """
+
         model = Reservation
         fields = '__all__'
         extra_kwargs = {
@@ -19,17 +37,43 @@ class ReservationSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, data):
+        """
+        Validates the assistant field.
+
+        Checks if the assistant is present and has the correct role.
+        If not, raises a ValidationError.
+
+        Args:
+            data (dict): The data to validate.
+
+        Returns:
+            dict: The validated data.
+
+        Raises:
+            ValidationError: If the assistant is not present or does not have the correct role.
+        """
+
         assistant = data.get('assistant')
 
         if not assistant:
             raise serializers.ValidationError({"assistant": "An assistant is required."})
 
-        # Aseguramos que el 'assistant' tenga el rol adecuado
+        # We ensure that the 'assistant' has the correct role
         if assistant.role != 'assistant':
             raise serializers.ValidationError({"assistant": "The specified user is not an assistant."})
 
         return data
 
     def create(self, validated_data):
-        # La instancia de assistant ya está correctamente asignada gracias a PrimaryKeyRelatedField
+        """
+        Creates a new reservation instance.
+
+        Args:
+            validated_data (dict): The validated data to create the reservation with.
+
+        Returns:
+            Reservation: The created reservation instance.
+        """
+
+        # The assistant instance is already correctly assigned thanks to PrimaryKeyRelatedField
         return Reservation.objects.create(**validated_data)
--- backend/reservation/views.py
+++ backend/reservation/views.py
@@ -7,8 +7,28 @@ from .models import Reservation
 from .serializers import ReservationSerializer
 
 class ReservationList(APIView):
+    """
+    This view handles the retrieval and creation of reservations.
+
+    It inherits from the APIView class provided by Django Rest Framework.
+    It requires the user to be authenticated to access, hence the IsAuthenticated permission class.
+    """
+
     permission_classes = [IsAuthenticated]
+
     def post(self, request):
+        """
+        Handles the POST request to create a new reservation.
+
+        Args:
+            request (Request): The request object containing the reservation data.
+
+        Returns:
+            Response: A response object containing the serialized reservation data and status code.
+                      If the reservation data is valid and the reservation is created successfully, it returns a 201 status code.
+                      If the reservation data is not valid, it returns a 400 status code along with the error messages.
+        """
+
         serializer = ReservationSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
@@ -16,25 +36,83 @@ class ReservationList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
+        """
+        Handles the GET request to retrieve all reservations.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: A response object containing the serialized reservation data.
+                      The data is a list of all reservations, each represented as a dictionary.
+                      If the request is successful, it returns a 200 status code.
+        """
+
         reservations = Reservation.objects.all()
         serializer = ReservationSerializer(reservations, many=True, context={'request': request})
         return Response(serializer.data)
 
 class ReservationDetail(APIView):
+    """
+    This view handles the retrieval, update, and deletion of a specific reservation.
+
+    It inherits from the APIView class provided by Django Rest Framework.
+    It requires the user to be authenticated to access, hence the IsAuthenticated permission class.
+    """
+
     permission_classes = [IsAuthenticated]
 
     def get_object(self, pk):
+        """
+        Retrieves a reservation by its primary key (pk).
+
+        Args:
+            pk (int): The primary key of the reservation to retrieve.
+
+        Returns:
+            Reservation: The reservation object.
+
+        Raises:
+            Http404: If a reservation with the provided primary key does not exist.
+        """
+
         try:
             return Reservation.objects.get(pk=pk)
         except Reservation.DoesNotExist:
             raise Http404
 
     def get(self, request, pk, *args, **kwargs):
+        """
+        Handles the GET request to retrieve a specific reservation.
+
+        Args:
+            request (Request): The request object.
+            pk (int): The primary key of the reservation to retrieve.
+            *args, **kwargs: Additional arguments and keyword arguments.
+
+        Returns:
+            Response: A response object containing the serialized reservation data.
+                      If the request is successful, it returns a 200 status code.
+        """
+
         reservation = self.get_object(pk)
         serializer = ReservationSerializer(reservation)
         return Response(serializer.data)
 
     def put(self, request, pk):
+        """
+        Handles the PUT request to update a specific reservation.
+
+        Args:
+            request (Request): The request object containing the new reservation data.
+            pk (int): The primary key of the reservation to update.
+
+        Returns:
+            Response: A response object containing the serialized reservation data and status code.
+                      If the reservation data is valid and the reservation is updated successfully, it returns a 200 status code.
+                      If the reservation data is not valid, it returns a 400 status code along with the error messages.
+        """
+
         reservation = self.get_object(pk)
         serializer = ReservationSerializer(reservation, data=request.data, context={'request': request})
         if serializer.is_valid():
@@ -43,6 +121,17 @@ class ReservationDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
+        """
+        Handles the DELETE request to delete a specific reservation.
+
+        Args:
+            request (Request): The request object.
+            pk (int): The primary key of the reservation to delete.
+
+        Returns:
+            Response: A response object containing a success message and a 204 status code.
+        """
+
         reservation = self.get_object(pk)
         reservation.delete()
         return Response({'message': 'Successfully deleted reservation'}, status=status.HTTP_204_NO_CONTENT)
