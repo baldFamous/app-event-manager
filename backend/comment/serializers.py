353f9291087faee0f2c5880b95bc025d2e522bff
brodriguez <bastian3967a@gmail.com>
2024-05-03 19:53:03 -0400

    add docs to app comment

--- backend/comment/serializers.py
+++ backend/comment/serializers.py
@@ -3,14 +3,31 @@ from rest_framework import serializers
 from .models import Comment, Event
 from django.contrib.auth import get_user_model
 
-
+# Import the user model from Django's authentication system
 User = get_user_model()
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    A serializer for the Comment model.
+
+    Attributes:
+        author: A foreign key to the User model.
+        event: A foreign key to the Event model.
+    """
+
     author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=True)
     event = serializers.PrimaryKeyRelatedField(queryset=Event.objects.all(), required=True)
 
     class Meta:
+        """
+        Meta class for additional serializer configuration.
+
+        Attributes:
+            model: The model to be serialized.
+            fields: The fields to be included in the serialized representation.
+            extra_kwargs: Additional field options.
+        """
+
         model = Comment
         fields = '__all__'
         extra_kwargs = {
@@ -19,10 +36,31 @@ class CommentSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+        """
+        Define the behavior for creating a new Comment instance.
+
+        Args:
+            validated_data: The validated data to be used to create the Comment instance.
+
+        Returns:
+            The created Comment instance.
+        """
+
         validated_data['comment_date'] = date.today()
         return Comment.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
+        """
+        Define the behavior for updating an existing Comment instance.
+
+        Args:
+            instance: The Comment instance to be updated.
+            validated_data: The validated data to be used to update the Comment instance.
+
+        Returns:
+            The updated Comment instance.
+        """
+
         instance.comment = validated_data.get('comment', instance.comment)
         instance.save()
         return instance
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -7,35 +7,100 @@ from .models import Comment
 from .serializers import CommentSerializer
 
 class CommentList(APIView):
+    """
+    APIView for handling Comment list operations.
+
+    Attributes:
+        permission_classes: A list of permission classes the view should use.
+    """
+
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        """
+        Handle GET requests for the list of Comment instances.
+
+        Args:
+            request: The request instance.
+
+        Returns:
+            A Response with the serialized Comment data.
+        """
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        """
+        Handle POST requests for creating a Comment instance.
+
+        Args:
+            request: The request instance.
+
+        Returns:
+            A Response with the serialized Comment data if the request data is valid.
+            A Response with the serializer errors if the request data is not valid.
+        """
         serializer = CommentSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class CommentDetail(APIView):
+    """
+    APIView for handling individual Comment instances.
+
+    Attributes:
+        permission_classes: A list of permission classes the view should use.
+    """
+
     permission_classes = [IsAuthenticated]
 
     def get_object(self, pk):
+        """
+        Retrieve a Comment instance by its primary key.
+
+        Args:
+            pk: The primary key of the Comment instance.
+
+        Returns:
+            The Comment instance.
+
+        Raises:
+            Http404: If the Comment instance does not exist.
+        """
         try:
             return Comment.objects.get(pk=pk)
         except Comment.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
+        """
+        Handle GET requests for a Comment instance.
+
+        Args:
+            request: The request instance.
+            pk: The primary key of the Comment instance.
+
+        Returns:
+            A Response with the serialized Comment data.
+        """
         comment = self.get_object(pk)
         serializer = CommentSerializer(comment)
         return Response(serializer.data)
 
     def put(self, request, pk):
+        """
+        Handle PUT requests for a Comment instance.
+
+        Args:
+            request: The request instance.
+            pk: The primary key of the Comment instance.
+
+        Returns:
+            A Response with the serialized Comment data if the request data is valid.
+            A Response with the serializer errors if the request data is not valid.
+        """
         comment = self.get_object(pk)
         serializer = CommentSerializer(comment, data=request.data, context={'request': request}, partial=True)
         if serializer.is_valid():
@@ -44,6 +109,16 @@ class CommentDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
+        """
+        Handle DELETE requests for a Comment instance.
+
+        Args:
+            request: The request instance.
+            pk: The primary key of the Comment instance.
+
+        Returns:
+            A Response with a success message.
+        """
         comment = self.get_object(pk)
         comment.delete()
         return Response({'message': 'Successfully deleted comment'}, status=status.HTTP_204_NO_CONTENT)
