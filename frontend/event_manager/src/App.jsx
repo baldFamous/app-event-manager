e5eb1798fbad28be6732cead5933bb08b9c230b8
brodriguez <bastian3967a@gmail.com>
2024-05-13 17:07:44 -0400

    add help page

--- frontend/event_manager/src/App.jsx
+++ frontend/event_manager/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import EventPage from './pages/EventPage';
 import './App.css';
 import OrganizersPage from "./pages/OrganizersPage";
+import HelpPage from "./pages/HelpPage";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/events" element={<EventPage />} />
                     <Route path="/organizers" element={<OrganizersPage/>} />
+                    <Route path="/help" element={<HelpPage />} />
                 </Routes>
             </div>
         </Router>
---
+++ frontend/event_manager/src/pages/HelpPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import './HelpPage.css';
+
+function HelpPage() {
+    return (
+        <div>
+            <NavBar />
+            <div className="help-section">
+                <h1>¿Cómo puedo ayudarte?</h1>
+                <section>
+                    <h2>Propósito del Sitio</h2>
+                    <p>Este sitio está diseñado para permitir a los usuarios descubrir eventos, reservar entradas y obtener información sobre los organizadores de eventos. Nuestro objetivo es simplificar el proceso de encontrar y asistir a eventos de todo tipo.</p>
+                </section>
+                <section>
+                    <h2>Cómo Funciona</h2>
+                    <p>Los usuarios pueden buscar eventos por fecha y localidad, ver detalles sobre cada evento, y realizar reservaciones en línea. Los organizadores pueden crear y gestionar eventos, incluyendo descripciones, fechas y opciones de reserva.</p>
+                </section>
+                <section>
+                    <h2>Finalidad</h2>
+                    <p>La finalidad de nuestro sitio es proporcionar una plataforma accesible y fácil de usar que conecte a los organizadores de eventos con el público interesado. Queremos fomentar una mayor participación en eventos culturales, educativos y de entretenimiento.</p>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default HelpPage;
